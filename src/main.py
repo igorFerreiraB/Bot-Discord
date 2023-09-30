ede1a60ca9da7626e144a658fb52b47116e44e49
igorFerreiraB <igorfgb2006@gmail.com>
2023-09-30 13:09:45 -0300

    new changes

--- src/main.py
+++ src/main.py
@@ -1,17 +1,21 @@
 import discord
 import os
+from key import token
+
+
 
 class MyClient(discord.Client):
+
     async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+        print(f'{self.user} Conectou ao nosso servidor!')
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
-        if message.content.startswith('$hello'):
-            await message.channel.send('Hello!')
-        elif message.content.startswith('?regras'):
+        if message.content == ('$ola'):
+            await message.channel.send('olá!')
+        elif message.content == ('?regras'):
             await message.channel.send(
-                f"{message.author.name}{os.linesep}\
+                f"{message.author.mention.name}{os.linesep}\
                 \n1. É proibido usar sapatos de cores diferentes em dias pares.\
                 \n2. Todo mundo deve usar um chapéu de abacaxi às segundas-feiras.\
                 \n3. É ilegal falar com esquilos em voz alta.\
@@ -23,8 +27,18 @@ class MyClient(discord.Client):
                 \n9. É proibido contar piadas às quintas-feiras.\
                 \n10. Todo mundo deve usar uma gravata borboleta em forma de pena aos domingos."
             )
-intents = discord.Intents.default()
+        elif message.content == ('?nivel'):
+            await message.author.send("Nivel menininha")
+
+    async def member_join(self, member):
+        guild = member.guild
+        if guild.system_channel is not None:
+            mensagem = f"{member.mention} Acabou de entrar no {guild.name}"
+            await guild.system_channel.send(mensagem)
+
+# TOKEN = token.get("TOKEN")
+intents = discord.Intents.all()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run('')
+client.run("")
