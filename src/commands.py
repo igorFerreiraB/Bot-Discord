5d4050762a9529fc951380dc493c11153067ecb8
igorFerreiraB <igorfgb2006@gmail.com>
2023-10-09 10:47:21 -0300

    up

--- src/commands.py
+++ src/commands.py
@@ -14,20 +14,9 @@ MY_GUILD = discord.Object(id=1034181969409474682)
 class MyClient(discord.Client):
     def __init__(self, *, intents: discord.Intents):
         super().__init__(intents=intents)
-        # A CommandTree is a special type that holds all the application command
-        # state required to make it work. This is a separate class because it
-        # allows all the extra state to be opt-in.
-        # Whenever you want to work with application commands, your tree is used
-        # to store and work with them.
-        # Note: When using commands.Bot instead of discord.Client, the bot will
-        # maintain its own tree instead.
         self.tree = app_commands.CommandTree(self)
 
-    # In this basic example, we just synchronize the app commands to one guild.
-    # Instead of specifying a guild to every command, we copy over our global commands instead.
-    # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
     async def setup_hook(self):
-        # This copies the global commands over to your guild.
         self.tree.copy_global_to(guild=MY_GUILD)
         await self.tree.sync(guild=MY_GUILD)
 
@@ -94,21 +83,17 @@ async def dividir(interaction: discord.Interaction, primeiro_número: float, seg
 
 @client.tree.command()
 @app_commands.rename(text_to_send='text')
-@app_commands.describe(text_to_send='Text to send in the current channel')
+@app_commands.describe(text_to_send='Texto que sera enviado no canal atual da menssagem')
 async def send(interaction: discord.Interaction, text_to_send: str):
     await interaction.response.send_message(text_to_send)
 
 
-# To make an argument optional, you can either give it a supported default argument
-# or you can mark it as Optional from the typing standard library. This example does both.
 @client.tree.command()
-@app_commands.describe(member='The member you want to get the joined date from; defaults to the user who uses the command')
+@app_commands.describe(member='member')
 async def joined(interaction: discord.Interaction, member: Optional[discord.Member] = None):
-    """Says when a member joined."""
-    # If no member is explicitly provided then we use the command user here
+    
     member = member or interaction.user
 
-    # The format_dt function formats the date time into a human readable representation in the official client
     await interaction.response.send_message(f'{member.mention} entrou no dia {discord.utils.format_dt(member.joined_at)}')
 
 
@@ -128,7 +113,7 @@ async def show_join_date(interaction: discord.Interaction, member: discord.Membe
 async def report_message(interaction: discord.Interaction, message: discord.Message):
     # We're sending this response message with ephemeral=True, so only the command executor can see it
     await interaction.response.send_message(
-        f'Thanks for reporting this message by {message.author.mention} to our moderators.', ephemeral=True
+        f'Obrigado por denunciar esta mensagem por {message.author.mention} para nossa moderação.', ephemeral=False
     )
 
     # Handle report by sending it into a log channel
