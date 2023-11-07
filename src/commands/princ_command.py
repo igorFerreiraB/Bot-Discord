90669d385423af7b30cb7f2ede9db32abe9dd47d
Igor Ferreira <131207955+igorFerreiraB@users.noreply.github.com>
2023-11-07 17:35:44 -0300

    update

--- src/commands/princ_command.py
+++ src/commands/princ_command.py
@@ -1,12 +1,36 @@
 from discord.ext import commands
+from openai import OpenAI
+client = OpenAI()
 
+# ALL Discord Commands
 class CommandCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def ola(self, ctx):
-        await ctx.send('Olá!')
+    async def ola(self, *, ctx:str):
+        gtp_answer = response(ctx)
+        await ctx.send(gtp_answer)
 
 async def setup(bot):
-    await bot.add_cog(CommandCog(bot))   
+    await bot.add_cog(CommandCog(bot))  
+
+# OpenAI
+
+
+def response(messages):
+    message = [
+        {"role": "system", "content": str(messages)},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+
+    response = client.chat.completions.create(
+    model = "gpt-3.5-turbo",
+    messages = messages,
+    max_tokens = 1024
+    )
+
+    retorno = response.choices[0].message.content 
+    return retorno
--- src/main2.py
+++ src/main2.py
@@ -1,5 +1,7 @@
 import discord
 import os
+from openai import OpenAI
+client = OpenAI()
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -9,13 +11,18 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
+my_secret_key = os.environ['token']
+# openai.api_key = os.environ['key_chatgpt'] 
 
 @bot.event
 async def on_ready():
+    print("-" * 30)
     print(f'Logged in as {bot.user.name}')
+    print("-" * 30)
+
     await bot.load_extension("commands.princ_command")
     await bot.tree.sync()
 
     print('All extensions loaded')
 
-bot.run(os.environ['token'])
+bot.run(my_secret_key)
