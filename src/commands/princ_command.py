67dd46ae1366ce945e229a2d5249ac3850800b13
Igor Ferreira <131207955+igorFerreiraB@users.noreply.github.com>
2023-11-08 21:50:53 -0300

    update

--- src/commands/princ_command.py
+++ src/commands/princ_command.py
@@ -1,15 +1,11 @@
-# import os
 import discord
 from discord.ext import commands
-# import openai
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 prefix = "!"
 bot = commands.Bot(command_prefix=prefix, intents=intents)
-
-# openai.api_key = os.environ.get("key_chatgpt")
 
 # ALL Discord Commands
 class CommandCog(commands.Cog):
@@ -23,28 +19,3 @@ class CommandCog(commands.Cog):
 
 async def setup(bot):
     await bot.add_cog(CommandCog(bot))  
-
-# # OpenAI
-# class CommandOpenai(commands.Cog):
-#     def __init__(self, bot):
-#         self.bot = bot
-
-#     @commands.command()
-#     async def ask(ctx, *, question):
-#         try:
-#             conversation = [
-#                 {"role": "system", "content": "Você é um bot de bate-papo."},
-#                 {"role": "user", "content": question}
-#             ]
-
-#             response = openai.ChatCompletion.create(
-#                 model="gpt-3.5-turbo",
-#                 messages=conversation
-#             )
-
-#             await ctx.send(response['choices'][0]['message']['content'])
-#         except Exception as error:
-#             await ctx.send(f"Ocorreu um erro: {str(error)}")
-  
-# async def setup2(bot):
-#     await bot.add_cog(CommandOpenai(bot))  
